42578fd0257abfe1cdb678cfe73c3bdc66776159
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2023-01-08 11:34:54 +0530

    Add test for repository

---
+++ repository/user_repository_test.go
@@ -0,0 +1,65 @@
+package repository_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/amitshekhariitbhu/go-backend-clean-architecture/domain"
+	"github.com/amitshekhariitbhu/go-backend-clean-architecture/mongo/mocks"
+	"github.com/amitshekhariitbhu/go-backend-clean-architecture/repository"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestCreate(t *testing.T) {
+
+	var databaseHelper *mocks.Database
+	var collectionHelper *mocks.Collection
+
+	databaseHelper = &mocks.Database{}
+	collectionHelper = &mocks.Collection{}
+
+	collectionName := domain.CollectionUser
+
+	mockUser := &domain.User{
+		ID:       primitive.NewObjectID(),
+		Name:     "Test",
+		Email:    "test@gmail.com",
+		Password: "password",
+	}
+
+	mockEmptyUser := &domain.User{}
+	mockUserID := primitive.NewObjectID()
+
+	t.Run("success", func(t *testing.T) {
+
+		collectionHelper.On("InsertOne", mock.Anything, mock.AnythingOfType("*domain.User")).Return(mockUserID, nil).Once()
+
+		databaseHelper.On("Collection", collectionName).Return(collectionHelper)
+
+		ur := repository.NewUserRepository(databaseHelper, collectionName)
+
+		err := ur.Create(context.Background(), mockUser)
+
+		assert.NoError(t, err)
+
+		collectionHelper.AssertExpectations(t)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		collectionHelper.On("InsertOne", mock.Anything, mock.AnythingOfType("*domain.User")).Return(mockEmptyUser, errors.New("Unexpected")).Once()
+
+		databaseHelper.On("Collection", collectionName).Return(collectionHelper)
+
+		ur := repository.NewUserRepository(databaseHelper, collectionName)
+
+		err := ur.Create(context.Background(), mockEmptyUser)
+
+		assert.Error(t, err)
+
+		collectionHelper.AssertExpectations(t)
+	})
+
+}
